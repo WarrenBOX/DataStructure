0e88c5a5ad8019ca4b76761b73d2a364e15f0db6
WarrenBOX <wujiahao058@gmail.com>
2022-07-12 01:09:25 -0700

    add push method to priorityQueue

---
+++ src/main/java/com/tunbobo/PriorityQueue/PriorityQueueDemo.java
@@ -0,0 +1,47 @@
+package com.tunbobo.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+
+    static class Node {
+        int value;
+        int priority;
+        Node next;
+
+        public Node(int value, int priority) {
+            this.value =value;
+            this.priority = priority;
+        }
+    }
+
+    Node head = null;
+
+    /**push: insert a new element
+     *
+     * **/
+    public void push(int value, int priority) {
+        //If it is an empty priority queue
+        if (head == null) {
+            head = new Node(value,priority);
+        }
+        //If the head is not null, need a pointer points to head node
+        Node current = head;
+        //create a new node
+        Node newNode = new Node(value,priority);
+        //If the priority is higher than the head
+        if (priority > head.priority) {
+            newNode.next = head;
+            this.head = newNode;
+        }
+        //Else compare to the next node,stops when hit the tail(current.next == null)
+        while(current.next!=null && current.next.priority > priority) {
+            // move to the next node
+            current = current.next;
+        }
+        //connect to the next node first
+        newNode.next = current.next;
+        //current node connect to the new node
+        current.next = newNode;
+        }
+
+}
